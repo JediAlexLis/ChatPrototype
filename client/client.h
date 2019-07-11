72edf232e5c1a32fc42537994f3a4c7cc0e8f8b6
JediAlexLis <alisitzin@gmail.com>
2019-07-11 11:34:58 +0300

    add new file

---
+++ client/client.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   client.h
+ * Author: Alexander Lisitzin
+ *
+ * Created on July 11, 2019, 8:31 AM
+ */
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* CLIENT_H */
+
---
+++ client/main.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.c
+ * Author: Alexander Lisitzin
+ *
+ * Created on July 11, 2019, 8:30 AM
+ */
+
+#include "client.h"
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
