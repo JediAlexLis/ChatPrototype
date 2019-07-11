91f4d4e7dab9072ede820336b9097395061afda5
JediAlexLis <alisitzin@gmail.com>
2019-07-11 18:20:11 +0300

    added new empty definitions and the previous code from main.c is placed here

--- client/client.c
+++ client/client.c
@@ -1,5 +1,8 @@
 #include "client.h"
 
+int sock;
+char command[LENGTH_COMMAND];
+
 // trimming \n
 void trim_string (char * string) {
     int i = 0;
@@ -9,6 +12,49 @@ void trim_string (char * string) {
             string = '\0';
             break;
         }
+    }
+}
+
+// connecting to the server
+int connect_server() {
+    
+}
+// log in
+int login() {
+    
+}
+// registration
+int registration() {
+    
+}
+
+// main menu
+void main_menu() {
+    printf ("Welcome to ChatPrototype!\n");
+    printf ("To find out which command is referred to which action, enter /help\n");
+    printf (MSG_ENTER_COMMAND);
+    
+    while (fgets (command, LENGTH_COMMAND, stdin) != NULL) {
+        trim_string(command);
+        
+        if (strcmp (command, "/help") == 0)
+            help_main_menu();
+        else if (strcmp (command, "/exit") == 0)
+            break;
+        else {
+            if (strcmp (command, "1") == 0)
+                main_chat();
+            else if (strcmp (command, "2") == 0)
+                group_chat();
+            else if (strcmp (command, "3") == 0)
+                create_group();
+            else if (strcmp (command, "4") == 0)
+                quit_group();
+            else
+                printf (MSG_ERROR_COMMAND);
+        }
+        
+        printf (MSG_ENTER_COMMAND);
     }
 }
 
