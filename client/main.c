a699dad367b4a238b6753faf7460968ae5438dd4
JediAlexLis <alisitzin@gmail.com>
2019-07-11 18:18:29 +0300

    replaced code to client.c and new code is being written

--- client/main.c
+++ client/main.c
@@ -7,38 +7,62 @@
 
 #include "client.h"
 
-// main menu
+int sock;
+char command[LENGTH_COMMAND] = {};
+
+// entrance to the program
 int main(void) {
-    char command[LENGTH_COMMAND] = {};
+    int connect_status;
     
-    printf ("Welcome to ChatPrototype!\n");
-    printf ("To find out which command is referred to which action, enter /help\n");
-    printf (MSG_ENTER_COMMAND);
-    
-    while (fgets (command, LENGTH_COMMAND, stdin) != NULL) {
-        trim_string(command);
+    printf (MSG_CONNECTING);
+    while ((connect_status = connect_server()) != 0) {
+        printf (MSG_ERR_CONNECTION);
+        printf (MSG_ENTER_WHEN_ERR_CONNECTION);
         
-        if (strcmp (command, "/help") == 0) {
-            help_main_menu();
-        } else if (strcmp (command, "/exit") == 0) {
-            break;
-        } else {
-            if (strcmp (command, "1") == 0) {
-                main_chat();
-            } else if (strcmp (command, "2") == 0) {
-                group_chat()
-            } else if (strcmp (command, "3") == 0) {
-                create_group();
-            } else if (strcmp (command, "4") == 0) {
-                quit_group();
-            } else {
-                printf (MSG_ERROR_COMMAND);
+        // choosing an action
+        while (1) {
+            if (fgets (command, LENGTH_COMMAND, stdin) == NULL) {
+                printf (MSG_ERROR_ENTER);
+                return (EXIT_FAILURE);
             }
+            if (strcmp (command, "0") == 0)
+                return (EXIT_SUCCESS);
+            else if (strcmp (command, "1") == 0)
+                break;
+            else
+                printf (MSG_ERROR_COMMAND);
+            
+            printf (MSG_ENTER_WHEN_ERR_CONNECTION);
         }
         
-        printf (MSG_ENTER_COMMAND);
+        printf (MSG_CONNECTING);
     }
     
+    printf (MSG_SUCCESSFUL_CONNECTION);
+    printf (MSG_ENTER_WHEN_CONNECTED);
+    
+    
+    
+    /*
+    printf ("If you have an account, enter 1 and login\n");
+    printf ("Otherwise, enter 2 to register a new account\n");
+    printf ("If you want to leave this program, enter /exit\n");
+    printf (MSG_ENTER_COMMAND);
+    while (fgets (command, LENGTH_COMMAND, stdin) != NULL) {
+        if (strcmp (command, "/exit") == 0)
+            break;
+        else {
+            if (strcmp (command, "1") == 0)
+                login();
+            else if (strcmp (command, "2") == 0)
+                registration();
+            else
+                printf (MSG_ERROR_COMMAND);
+        }
+        
+    }
+    */
+
     return (EXIT_SUCCESS);
 }
 
