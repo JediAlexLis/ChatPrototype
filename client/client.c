27358d6ecdd58bbb24188967ca844a73e74c69b5
JediAlexLis <alisitzin@gmail.com>
2019-07-11 12:17:13 +0300

    added empty definitions

--- client/client.c
+++ client/client.c
@@ -1,2 +1,94 @@
 #include "client.h"
 
+// trimming \n
+void trim_string (char * string) {
+    int i = 0;
+    while (i++ < LENGTH_COMMAND) {
+        if (string[i] = '\n')
+        {
+            string = '\0';
+            break;
+        }
+    }
+}
+
+// connecting to the main chat
+void main_chat() {
+    
+}
+// connecting to a group chat
+void group_chat() {
+    
+}
+// creating a new group
+void create_group() {
+    
+}
+// quitting from a group
+void quit_group() {
+    
+}
+
+// sending a message
+void send_message() {
+    
+}
+// sending a file
+void send_file() {
+    
+}
+// changing a message
+void change_message() {
+    
+}
+// deleting a message
+void delete_message() {
+    
+}
+
+// inviting a new member of a group
+void invite_member() {
+    
+}
+
+// choosing an object of an action
+void choose() {
+    
+}
+
+// calling help for main menu
+void help_main_menu() {
+    
+}
+// calling help for a main chat
+void help_main_chat() {
+    
+}
+// calling help for a group chat
+void help_group_chat() {
+    
+}
+// calling help for sending messages
+void help_send_message() {
+    
+}
+// calling help for sending files
+void help_send_file() {
+    
+}
+// calling help for changing messages
+void help_change_message() {
+    
+}
+// calling help for deleting messages
+void help_delete_message() {
+    
+}
+// calling help for inviting of new members of a group
+void help_invite_member() {
+    
+}
+// calling help for choosing an object of an action
+void help_choose() {
+    
+}
